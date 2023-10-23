31535ef3ad3ec7f87c6edc81724e60559325c884
Bar Snir <barsnir@Bars-MBP.lan>
2023-10-23 21:35:19 +0100

    added csv for market info model

--- libs/connectors/mysql/__init__.py
+++ libs/connectors/mysql/__init__.py
@@ -6,6 +6,7 @@ class MySqlConnector:
     def __init__(self, logger: Logger):
         self._connector = self._get_connector()
         self._cursor = self._get_cursor()
+        self.table_names = []
         self.logger = logger
 
     def _get_connector(self)-> mysql.connector.MySQLConnection:
@@ -16,20 +17,21 @@ class MySqlConnector:
         )
     
     def _get_cursor(self)-> CursorBase:
-        return self._connector.cursor()
+        return self._connector.cursor(buffered=True)
     
     def create_database(self)-> None:
         try:
-            self._cursor.execute(f"CREATE DATABASE {os.getenv('DATABSE')}")
-            self.logger.info(f"Database {os.getenv('DATABSE')} created!")
+            self._cursor.execute(f"CREATE DATABASE {os.getenv('DATABASE')}")
+            self.logger.info(f"Database {os.getenv('DATABASE')} created!")
         except Exception as e:
             self.logger.warn(e)
 
     def create_tables(self)-> None:
-        self._cursor.execute(f"USE {os.getenv('DATABSE')}")
-        with open("configs/database.json") as databse_config:
-            for table in json.load(databse_config).get('tables'):
+        self._cursor.execute(f"USE {os.getenv('DATABASE')}")
+        with open("configs/database.json") as database_config:
+            for table in json.load(database_config).get('tables'):
                 table_name = table.get('name')
+                self.table_names.append(table_name)
                 columns = self._construct_columns_statement(table.get('columns'))
                 try:
                     self._cursor.execute(f"CREATE TABLE {table_name} ({columns})")
@@ -45,3 +47,11 @@ class MySqlConnector:
         for column in columns:
             column_statement = column_statement + column
         return column_statement
+    
+    def get_schemas(self)-> list:
+        schemas_list = []
+        for table_name in self.table_names:
+            self._cursor.execute(f"DESCRIBE {table_name}")
+            schema = self._cursor.fetchall()
+            schemas_list.append({'table_name': table_name, 'schema': schema})
+        return schemas_list
--- steps/a_setup_database.py
+++ steps/a_setup_database.py
@@ -5,9 +5,11 @@ MODULE_MESSAGE = 'Step A | Starting to setup MySQL database'
 LOGGER_NAME = 'mysql_setup'
 
 def process():
-    # Step 1: Create Connector's handler
     logger = ColorLogger(LOGGER_NAME).get_logger()
     logger.info(MODULE_MESSAGE)
     mysql_connector = MySqlConnector(logger)
     mysql_connector.create_database()
     mysql_connector.create_tables()
+    # schemas = mysql_connector.get_schemas()
+
+    
