1128713776056c701981da729d0f392a2303f6de
Bar Snir <barsnir@Bars-MBP.lan>
2023-10-28 03:11:26 +0100

    after long hustle, first pipe fro file system to mysql is exsitsgit add .

--- flink_process/generate_market_info.py
+++ flink_process/generate_market_info.py
@@ -1,4 +1,4 @@
-import os
+import time
 from random import randrange
 from pyflink.table.udf import udf
 from pyflink.table import ( 
@@ -8,8 +8,7 @@ from pyflink.table import (
     expressions as F
 )
 
-
-@udf(result_type='BIGINT')
+@udf(result_type=DataTypes.BIGINT())
 def calc_gemetria(text):
     total = 0
     clean_text = text.replace(' ','') \
@@ -30,13 +29,14 @@ def calc_gemetria(text):
     return total
 
 def get_jars_path():
-    return f'file:///Users/barsnir/Desktop/flink_playground/flink-1.17.1/opt/'
+    return f'file:///opt/flink/opt/'
 
 def get_jars_full_path() -> str:
   jars_path = get_jars_path()
   jars = [
     'flink-connector-jdbc-3.1.0-1.17.jar;',
-    'mysql-connector-java-5.1.9.jar'
+    'mysql-connector-java-5.1.9.jar;',
+    'flink-python-1.17.1.jar'
   ]
   full_str = ''
   for jar in jars:
@@ -44,10 +44,11 @@ def get_jars_full_path() -> str:
   return full_str
 
 def process():
-
     environment_settings = EnvironmentSettings.in_streaming_mode()
     t_env = TableEnvironment.create(environment_settings)
-    t_env.get_config().set('pipeline.jars',get_jars_full_path())
+    t_env.get_config().set('pipeline.jars',get_jars_full_path()) \
+    .set('python.fn-execution.bundle.time', '100000') \
+    .set('python.fn-execution.bundle.size', '10') 
 
     source_ddl = """
         CREATE TABLE MarketInfoInit (
@@ -75,7 +76,11 @@ def process():
             'url' = 'jdbc:mysql://db:3306/production',
             'table-name' = 'MarketInfo',
             'username'='root',
-            'password'='password'
+            'password'='password',
+            'sink.parallelism' = '4',
+            'sink.buffer-flush.interval' = '0',
+            'sink.buffer-flush.max-rows' = '10',
+            'sink.max-retries' = '10'
         );
     """
     t_env.execute_sql(source_ddl)
@@ -85,21 +90,20 @@ def process():
         F.col('make').alias('ManufacturerText'),
         F.col('model').alias('ModelText'),
         F.col('year').cast(DataTypes.INT()).alias('Year')
-    ).filter(F.col('ManufacturerText') != 'make').execute().print()
-    # market_info_table.select(
-    #     (
-    #         calc_gemetria(F.concat(F.col('ManufacturerText'), F.col('ModelText'))) \
-    #         + \
-    #         F.col('Year')
-    #         +
-    #         randrange(1, 1000000)
-    #     ).alias('MarketInfoId'),
-    #     calc_gemetria(F.col('ManufacturerText')).alias('ManufacturerId'),
-    #     F.col('ManufacturerText'),
-    #     calc_gemetria(F.concat(F.col('ManufacturerText'), F.col('ModelText'))).alias('ModelId'),
-    #     F.col('ModelText'),
-    #     F.col('Year')
-    # ).execute_insert('MysqlSink').wait(100000000)
-
+    ).filter(F.col('ManufacturerText') != 'make')
+    market_info_table.select(
+        (
+            calc_gemetria(F.concat(F.col('ManufacturerText'), F.col('ModelText'))) \
+            + \
+            F.col('Year')
+            +
+            randrange(1, 1000000)
+        ).alias('MarketInfoId'),
+        calc_gemetria(F.col('ManufacturerText')).alias('ManufacturerId'),
+        F.col('ManufacturerText'),
+        calc_gemetria(F.concat(F.col('ManufacturerText'), F.col('ModelText'))).alias('ModelId'),
+        F.col('ModelText'),
+        F.col('Year')
+    ).execute_insert('MysqlSink').wait(50000)
 if __name__ == "__main__":
     process()
---
+++ images/pyflink/.Dockerfile
@@ -0,0 +1,20 @@
+
+FROM apache/flink:1.17.1
+
+# The liblzma-dev is important to apache beam runtime
+RUN apt-get update -y && \
+apt-get install -y build-essential liblzma-dev libssl-dev zlib1g-dev libbz2-dev libffi-dev && \
+wget https://www.python.org/ftp/python/3.9.7/Python-3.9.7.tgz && \
+tar -xvf Python-3.9.7.tgz && \
+cd Python-3.9.7 && \
+./configure --without-tests --enable-shared && \
+make -j6 && \
+make install && \
+ldconfig /usr/local/lib && \
+cd .. && rm -f Python-3.9.7.tgz && rm -rf Python-3.9.7 && \
+ln -s /usr/local/bin/python3 /usr/local/bin/python && \
+apt-get clean && \
+rm -rf /var/lib/apt/lists/*
+
+# install PyFlink
+RUN pip3 install apache-flink==1.17.1
