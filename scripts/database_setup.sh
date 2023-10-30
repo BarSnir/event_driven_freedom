75b7ae848fd82281e256e80c5d0944a08ce72d66
Bar Snir <barsnir@Bars-MBP.lan>
2023-10-30 17:15:13 +0000

    done with setup database & datasets

--- scripts/database_setup.sh
+++ scripts/database_setup.sh
@@ -3,28 +3,27 @@ sleep 30
 echo 'Step A || Creating databases & tables'
 python3 /opt/flink/project/main.py
 
-echo 'Step B || Generating datasets in 20s with Flink batch opersions'
-sleep 20
+echo 'Step B || Generating datasets in 20s with Flink batch opersions!'
+echo 'Invited to take a look at job statuses at http://localhost:8081'
 # Yes the order is necessary :)
 declare -a pyflink_scripts=(
-    "generate_market_info"
-    "generate_vehicles"
-    "generate_orders" 
-    "generate_images"
-    "generate_customers"
-    "generate_auth_types"
-    "generate_improves"
-    "generate_media_type"
-    "generate_statuses"
+    [1]="generate_market_info"
+    [2]="generate_vehicles"
+    [3]="generate_orders" 
+    [4]="generate_images"
+    [5]="generate_customers"
+    [6]="generate_auth_types"
+    [7]="generate_improves"
+    [8]="generate_media_type"
+    [9]="generate_statuses"
 )
-# 1> /dev/null
 
-for script in ${pyflink_scripts[@]}; do
+for key in "${!pyflink_scripts[@]}"; do
+    echo Job ${pyflink_scripts[$key]} Is Running! $key out of 9.
     ./bin/flink run \
       --jobmanager jobmanager:8081 \
       -pyclientexec /usr/local/bin/python3 \
       -pyexec /usr/local/bin/python3 \
-      -py /opt/flink/ops/${script}.py 
-    echo ${script} Done!
-    sleep 10
+      -py /opt/flink/ops/${pyflink_scripts[$key]}.py &>/dev/null
+    echo The job ${pyflink_scripts[$key]} is Done!
 done
