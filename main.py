f2dec6400f6c8243afd4c4d9d972e01e4106f144
Bar Snir <bar1991snir@gmail.com>
2024-02-01 15:02:35 -0800

    adding new stream section

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from libs.utils.logger import ColorLogger
 from steps import (
     setup_database,
     process_batch_dataset,
-    process_stream_datasets
+    process_stream_dataset
 )
 
 if __name__ == "__main__":
     logger = ColorLogger("event_driven_freedom").get_logger()
     setup_database.process(logger)
     process_batch_dataset.process(logger)
-    process_stream_datasets.process(logger)
+    process_stream_dataset.process(logger)
---
+++ steps/process_stream_dataset.py
@@ -0,0 +1,6 @@
+
+def process(logger):
+    pass
+    # CURL for create connector.
+    # Wakeup flink stream clusters.
+    # Run flink stream workflow.
