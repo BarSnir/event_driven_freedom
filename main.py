d299cc2267b101e860000dcc31393facf1a06629
Bar Snir <bar1991snir@gmail.com>
2024-02-01 14:59:12 -0800

    adding streaming module for automation

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from libs.utils.logger import ColorLogger
 from steps import (
     setup_database,
     process_batch_dataset,
+    process_stream_datasets
 )
 
 if __name__ == "__main__":
     logger = ColorLogger("event_driven_freedom").get_logger()
     setup_database.process(logger)
     process_batch_dataset.process(logger)
-    
+    process_stream_datasets.process(logger)
