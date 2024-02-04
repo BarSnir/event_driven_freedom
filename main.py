0b3070fdfa47379e76aaf8275bf296d26ba9e8ed
Bar Snir <bar1991snir@gmail.com>
2024-02-03 22:27:51 -0800

    added connector submitting

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from libs.utils.logger import ColorLogger
 from steps import (
     setup_database,
@@ -6,6 +7,7 @@ from steps import (
 )
 
 if __name__ == "__main__":
+    time.sleep(10)
     logger = ColorLogger("event_driven_freedom").get_logger()
     setup_database.process(logger)
     process_batch_dataset.process(logger)
--- steps/process_stream_dataset.py
+++ steps/process_stream_dataset.py
@@ -1,6 +1,18 @@
+import json, requests,os
+from libs.utils.logger import ColorLogger
+
+MODULE_MESSAGE = 'Step C || Generating Debezium & Apache Flink stream operations!'
+CONNECT_URL = 'http://kafka-connect:8083/connectors/debezium_pyflink_v1/config'
 
 def process(logger):
-    pass
-    # CURL for create connector.
+    ColorLogger.log_new_step_dashes(logger)
+    logger.info(MODULE_MESSAGE)
+    ColorLogger.log_new_step_dashes(logger)
+    try:
+        with open(f"{os.getenv('CONNECTOR_CONFIG_PATH')}") as connector_config_file:
+            connector_config = json.load(connector_config_file)
+            requests.put(CONNECT_URL,data=json.dumps(connector_config))
+    except:
+        pass
     # Wakeup flink stream clusters.
     # Run flink stream workflow.
