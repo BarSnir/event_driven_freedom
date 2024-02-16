bc939a3251d4b89436c2dbda806d470a1477c471
Bar Snir <bar1991snir@gmail.com>
2024-02-16 18:25:08 +0200

    working until debezium config

--- steps/process_stream_dataset.py
+++ steps/process_stream_dataset.py
@@ -1,4 +1,6 @@
-import json, requests,os
+import os
+from libs.connectors.kafka_connect import KafkaConnectConnector
+from requests.exceptions import RequestException
 from libs.utils.logger import ColorLogger
 
 MODULE_MESSAGE = 'Step C || Generating Debezium & Apache Flink stream operations!'
@@ -7,20 +9,14 @@ def process(logger):
     ColorLogger.log_new_step_dashes(logger)
     logger.info(MODULE_MESSAGE)
     ColorLogger.log_new_step_dashes(logger)
+    kafka_connect_connector = KafkaConnectConnector()
     try:
-        with open(f"{os.getenv('CONNECTOR_CONFIG_PATH')}") as connector_config_file:
-            connector_config = json.load(connector_config_file)
-            headers={
-                'Content-type': 'application/json'
-            }
-            logger.debug(connector_config)
-            logger.debug(f"{os.getenv('CONNECT_URL')}/connectors")
-            requests.post(
-                f"{os.getenv('CONNECT_URL')}/connectors/",
-                json=connector_config,
-                headers=headers
-            )
-    except:
-        pass
+        with open(f"{os.getenv('DEBEZIUM_CONFIG_FILE_PATH')}") as debezium_config_file:
+            response = kafka_connect_connector.post_new_connector(logger, debezium_config_file)
+            if response.status_code == 400:
+                logger.error(response.json())
+                raise RequestException
+    except RequestException as req_exception:
+        logger.error("Pay attention to connector request.")
     # Wakeup flink stream clusters.
     # Run flink stream workflow.
