bb4ed582633bc39101538eaf169f4e7a24a05718
Bar Snir <bar1991snir@gmail.com>
2024-03-03 18:00:43 +0200

    added fileds, now need to debug

--- flink_process/transform_search_documents.py
+++ flink_process/transform_search_documents.py
@@ -19,17 +19,57 @@ def log_processing():
     full_ads_table.filter(F.col('status_id') == 1)
     full_ads_table.select(
         F.col('order_id'),
+        F.col('site_token'),
+        F.col('status_id'),
+        F.col('price'),
+        F.col('market_price'),
         F.row(
+            F.col('manufacturer_id'),
+            F.col('manufacturer_text'),
+            F.col('model_id'),
+            F.col('model_text'),
+            F.col('year'),
             F.col('km'),
+            F.col('horse_power'),
             F.col('prev_owner_number').cast(DataTypes.INT()).alias('prev_owner_number'),
             F.col('year_on_road'),
+            F.col('test_date'),
             F.row(
+                F.col('fully_autonomic').cast(DataTypes.BOOLEAN()).alias('fully_autonomic'),
+                F.col('power_wheel').cast(DataTypes.BOOLEAN()).alias('power_wheel'),
+                F.col('cruse_control').cast(DataTypes.BOOLEAN()).alias('cruse_control'),
                 F.col('sun_roof').cast(DataTypes.BOOLEAN()).alias('sun_roof'),
-                F.col('magnesium_wheels').cast(DataTypes.BOOLEAN()).alias('magnesium_wheels')
-            ).alias('peripheral_equipment')
+                F.col('magnesium_wheels').cast(DataTypes.BOOLEAN()).alias('magnesium_wheels'),
+                F.col('air_bags'),
+                F.col('revers_sensors').cast(DataTypes.BOOLEAN()).alias('revers_sensors'),
+                F.col('abs').cast(DataTypes.BOOLEAN()).alias('abs'),
+                F.col('hybrid').cast(DataTypes.BOOLEAN()).alias('hybrid'),
+                F.col('doors').cast(DataTypes.INT()).alias('doors'),
+                F.col('environment_friendly_level').cast(DataTypes.INT()).alias('environment_friendly_level'),
+                F.col('security_test_level').cast(DataTypes.INT()).alias('security_test_level'),
+            ).alias('peripheral_equipment'),
+            F.row(
+                F.col('improve_id'),
+                F.col('parts_improved_list'),
+                F.col('stage_level').cast(DataTypes.INT()).alias('stage_level'),
+                F.col('stage_text')
+            ).alias('improvements'),
+            F.row(
+                F.col('available_disk_slot'),
+                F.col('usb_slot_type'),
+                F.col('usb_slots'),
+                F.col('is_touch_display')
+            ).alias('media')
         ).alias('vehicle_specs'),
         F.row(
-            FlinkUDFs.cast_to_array(F.col('images_urls')).alias('images_urls')
+            F.col('customer_id'),
+            F.concat(F.col('first_name'),'_',F.col('last_name')) \
+            .cast(DataTypes.STRING()).alias('full_name')
+        ).alias('customer_details'),
+        F.row(
+            F.col('vehicle_id'),
+            FlinkUDFs.cast_to_array(F.col('images_urls')).alias('images_urls'),
+            F.col('images_count').cast(DataTypes.INT()).alias('images_count'),
         ).alias('meta_data')
     ).execute_insert(search_documents_topic_connector.table_name).wait()
 
