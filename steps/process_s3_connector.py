e0d45d4b213a28e6ac304c26aad21aeb6ba4c4fd
Bar Snir <bar1991snir@gmail.com>
2024-03-23 19:35:26 +0200

    added automatic bucket creator script

--- steps/process_s3_connector.py
+++ steps/process_s3_connector.py
@@ -2,6 +2,7 @@ from requests.exceptions import RequestException
 from libs.utils.list import ListUtils
 from libs.utils.files import FileUtils
 from libs.utils.logger import ColorLogger
+from libs.connectors.minio import MinioHandler
 from libs.connectors.kafka_connect import KafkaConnectClient
 from libs.connectors.kafka_admin import KafkaAdminClientWrap
 
@@ -14,6 +15,7 @@ def process(logger):
     ColorLogger.log_new_step_dashes(logger)
     kafka_admin_client = KafkaAdminClientWrap(logger)
     kafka_connect_client = KafkaConnectClient()
+    minio_handler = MinioHandler(logger)
     try:
         config = FileUtils.get_json_file(FILE_PATH)
         logger.debug(config)
@@ -22,6 +24,7 @@ def process(logger):
         )
         logger.debug(topic_list)
         kafka_admin_client.find_topics(topic_list)
+        minio_handler.create_bucket()
         kafka_connect_client.post_new_connector(
             logger, config
         )
