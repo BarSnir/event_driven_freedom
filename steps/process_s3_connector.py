beb7ff657f8363df54af2476439e3d305a7b419f
Bar Snir <bar1991snir@gmail.com>
2024-03-23 18:38:51 +0200

    ready to test

--- steps/process_s3_connector.py
+++ steps/process_s3_connector.py
@@ -4,10 +4,9 @@ from libs.utils.files import FileUtils
 from libs.utils.logger import ColorLogger
 from libs.connectors.kafka_connect import KafkaConnectClient
 from libs.connectors.kafka_admin import KafkaAdminClientWrap
-from libs.connectors.elasticsearch import ElasticsearchConnector
 
 MODULE_MESSAGE = 'Step F || Generating Elasticsearch sink connector'
-INDEX_PATTERN = '/opt/flink/project/configs/index_pattern.json'
+FILE_PATH = '/opt/flink/project/configs/minio_sink.json'
 
 def process(logger):
     ColorLogger.log_new_step_dashes(logger)
@@ -18,7 +17,9 @@ def process(logger):
     try:
         config = FileUtils.get_json_file(FILE_PATH)
         logger.debug(config)
-        topic_list = config.get('config').get('topics')
+        topic_list = ListUtils.str_to_list(
+            config.get('config').get('topics')
+        )
         logger.debug(topic_list)
         kafka_admin_client.find_topics(topic_list)
         kafka_connect_client.post_new_connector(
